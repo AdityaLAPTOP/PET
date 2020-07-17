8d11489d9fc48dc42b259c25ab267a55fa139124
Aditya <61464659+AdityaLAPTOP@users.noreply.github.com>
2020-07-17 19:33:33 +0530

    Add files via upload

---
+++ PET/Virtual-Pet--1-master/sketch.js
@@ -0,0 +1,27 @@
+//Create variables here
+var dog, happyDog, database, foodS, foodStock;
+function preload()
+{
+  var a = loadImage("./images/dogImg.png")
+  var b = loadImage("./images/dogImg1.png")
+	//load images here
+}
+
+function setup() {
+	createCanvas(800, 700);
+  var dog=createSprite(10,200,250,250);
+foodStock=database.ref('food');
+foodStock.on("value",readStock)
+}
+
+
+function draw() {  
+if(keyWentDown(UP_ARROW)){
+  dog.addImage(a);
+}
+  drawSprites();
+  //add styles here
+
+}
+
+
